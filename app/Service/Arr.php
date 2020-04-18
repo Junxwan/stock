a3634bf1dad83b6d5a38429834ebe1a916b70416
Junx <hugh8845sam@gmail.com>
2020-04-18 22:32:24 +0800

    移除沒用method

--- app/Service/Arr.php
+++ app/Service/Arr.php
@@ -8,22 +8,6 @@ class Arr
      * @param array $data
      * @param string $key
      *
-     * @return array
-     */
-    public static function key(array $data, string $key): array
-    {
-        $d = [];
-        foreach ($data as $v) {
-            $d[$v[$key]] = $data;
-        }
-
-        return $d;
-    }
-
-    /**
-     * @param array $data
-     * @param string $key
-     *
      * @return string
      */
     public static function string(array $data, string $key): string
