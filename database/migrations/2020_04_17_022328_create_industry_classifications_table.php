d9b391d72f5cc85942ae209420543fa6b992e80a
Junx <hugh8845sam@gmail.com>
2020-04-18 13:11:20 +0800

    新增股票跟產業分類 table

---
+++ database/migrations/2020_04_17_022328_create_industry_classifications_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIndustryClassificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('industry_classifications', function (Blueprint $table) {
+            $table->char('code', 5)->primary()->comment('產業指數代號');
+            $table->string('name', 50)->comment('產業名稱');
+            $table->string('tw_name', 50)->comment('產業指數名稱');
+        });
+
+        DB::statement('ALTER TABLE `industry_classifications` COMMENT = "產業分類"');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('industry_classifications');
+    }
+}
---
+++ database/migrations/2020_04_17_023457_create_stocks_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->char('code', 4)->primary()->comment('代碼');
+            $table->string('name', 50)->comment('名稱');
+            $table->integer('capital')->comment('股本(千)');
+            $table->char('industry_code', 5)->comment('產業分類');
+            $table->char('classification', 50)->comment('細產業分類');
+            $table->tinyInteger('issued')->comment('上市:1 上櫃:2');
+            $table->date('twse_date')->nullable()->comment('上市日期');
+            $table->date('otc_date')->nullable()->comment('上櫃日期');
+            $table->date('creation_date')->nullable()->comment('成立日期');
+
+            $table->foreign('industry_code')->references('code')->on('industry_classifications');
+        });
+
+        DB::statement('ALTER TABLE `stocks` COMMENT = "股票"');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stocks');
+    }
+}
