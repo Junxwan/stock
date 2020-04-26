7be04f197743dc5726dfed8453713c793d80e3ed
Junx <hugh8845sam@gmail.com>
2020-04-26 11:32:26 +0800

    增加預設非開市日

--- app/Service/OpenDate.php
+++ app/Service/OpenDate.php
@@ -8,6 +8,31 @@ use Illuminate\Support\Carbon;
 
 class OpenDate
 {
+    private $close = [
+        '2020-01-01',
+        '2020-01-02',
+        '2020-01-20',
+        '2020-01-23',
+        '2020-01-24',
+        '2020-01-25',
+        '2020-01-26',
+        '2020-01-27',
+        '2020-01-28',
+        '2020-01-29',
+        '2020-01-30',
+        '2020-02-28',
+        '2020-04-02',
+        '2020-04-03',
+        '2020-04-04',
+        '2020-05-01',
+        '2020-06-25',
+        '2020-06-26',
+        '2020-10-01',
+        '2020-10-02',
+        '2020-10-09',
+        '2020-10-10',
+    ];
+
     /**
      * @var OpenDateRepository
      */
@@ -31,14 +56,18 @@ class OpenDate
     public function create(string $year): bool
     {
         $date = Carbon::createFromFormat('Y-m-d', $year . '-01-01');
-
         $range = CarbonPeriod::create($date->toDateString(), $date->endOfYear()->toDateString());
 
         foreach ($range as $d) {
+            $isOpen = $d->isWeekday();
+            if (in_array($d->toDateString(), $this->close)) {
+                $isOpen = false;
+            }
+
             $insert[] = [
                 'date' => $d->toDateString(),
                 'week' => $d->weekday(),
-                'open' => $d->isWeekday(),
+                'open' => $isOpen,
             ];
         }
 
